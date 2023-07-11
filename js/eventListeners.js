2aa0e4468ab7d44e1144f5aa7ab45a21e38e7982
ilyha-t <ilyhanef@mail.ru>
2023-07-11 18:08:27 +0300

    Fix bag if input empty

--- js/eventListeners.js
+++ js/eventListeners.js
@@ -7,20 +7,23 @@ let getRepos = autocompleteInput(fetchGitRepos, 500);
 
 
 repoInput.addEventListener('input', function (e) {
-    getRepos(e.target.value)
-        .then(response => {
-            let repos = showAutoComplete(showAutocompleteArea, response);
-            let autocompleteItems = document.querySelectorAll('.autocomplete__list__item');
-            if (repos.length > 0) {
-                autocompleteItems.forEach(curRepo => curRepo.addEventListener('click', function(e) {
+    if(e.target.value.trim()) {
+        getRepos(e.target.value)
+            .then(response => {
+                let repos = showAutoComplete(showAutocompleteArea, response);
+                let autocompleteItems = document.querySelectorAll('.autocomplete__list__item');
+                if (repos.length > 0) {
+                    autocompleteItems.forEach(curRepo => curRepo.addEventListener('click', function(e) {
                         createCardRepo(e.target.data ,repos);
                         showAutocompleteArea.classList.add('hidden');
                         infoMessage.classList.add('hidden');
                         repoInput.value = '';
-                }));
-            }
-        })
-        .catch(error => console.log(error));
+                    }));
+                }
+            })
+            .catch(error => console.log(error));
+    }
+
 });
 
 
